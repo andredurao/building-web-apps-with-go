db19bec0897d106341e45fae849f74ab996f0858
André Durão <andreluisdurao@gmail.com>
2017-01-03 09:14:20 -0200

    Implementing the second example from the book

--- example1-fileserver/fileserver.go
+++ example1-fileserver/fileserver.go
@@ -1,7 +1,18 @@
 package main
 
-import "net/http"
+// Added the log interface to check what is being served
+import (
+	"log"
+	"net/http"
+)
+
+func loggingHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.URL.Path)
+		h.ServeHTTP(w, r)
+	})
+}
 
 func main() {
-	http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
+	http.ListenAndServe(":8080", loggingHandler(http.FileServer(http.Dir("."))))
 }
---
+++ example2-creating-a-basic-web-app/markdown.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/russross/blackfriday"
+	"net/http"
+)
+
+//TODO: Use the same LOG from example1
+func main() {
+	http.HandleFunc("/markdown", GenerateMarkdown)
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.ListenAndServe(":8080", nil)
+}
+
+func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
+	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+	rw.Write(markdown)
+}
